36b6578dee44a6de5ca50fcf0d469a0bc2c45d04
Madjidbg7 <madjidbinance1@gmail.com>
2022-09-06 00:02:32 +0200

    changement

--- demo.php
+++
@@ -1,7 +0,0 @@
-je voudrais savoir combien est egale 93 + 19 ?
-<?php
-$reponsephrase = ' est egale a ';
-$chiffre93 = 93;
-$chiffre19 = 19;
-echo '93 + 19 ' . $reponsephrase . ( $chiffre93 + $chiffre19);
-?>
